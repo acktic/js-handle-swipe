244263806585c71bbbb4b01241cc5a2193abcb65
ack <acktic@gmail.com>
2021-06-14 09:53:36 -0700

    Create baseFunctions.js

---
+++ baseFunctions.js
@@ -0,0 +1,20 @@
+
+      document.addEventListener('touchstart', (evt) => {
+          touchstartX = evt.changedTouches[0].screenX
+          touchstartY = evt.changedTouches[0].screenY;
+        },
+        { passive: true }
+      );
+
+      document.addEventListener('touchend', (evt) => {
+          touchendX = evt.changedTouches[0].screenX;
+          touchendY = evt.changedTouches[0].screenY;
+          handleGuide();
+        },
+        { passive: true }
+      );
+      
+      let handleSwipe = function () {
+        if (touchendX - 50 > touchstartX) { }
+        else if (touchendX + 50 < touchstartX) { }
+}
